1ea9166ed50a71bc6a2f2fc8a3f836875cd66b66
Caio Costa <caio.costa@grupocriar.com>
2026-03-23 16:40:56 -0300

    Initial commit

---
+++ app/src/main/java/com/dev/caiovinicius/planner/ui/InitialLoadingFragment.kt
@@ -0,0 +1,39 @@
+package com.dev.caiovinicius.planner.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dev.caiovinicius.planner.R
+import com.dev.caiovinicius.planner.databinding.FragmentInitialLoadingBinding
+
+class InitialLoadingFragment : Fragment() {
+
+    private var _binding: FragmentInitialLoadingBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentInitialLoadingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            // Todo: lógica da tela de laoding inicial (ir para cadastro de usuário ou home)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+
+    }
+
+}
---
+++ app/src/main/java/com/dev/caiovinicius/planner/ui/UpdatePlannerActivityDialogFragment.kt
@@ -0,0 +1,38 @@
+package com.dev.caiovinicius.planner.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dev.caiovinicius.planner.databinding.FragmentUpdatePlannerActivityDialogBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class UpdatePlannerActivityDialogFragment : BottomSheetDialogFragment() {
+
+    private var _binding: FragmentUpdatePlannerActivityDialogBinding? = null
+    private val binding get() = _binding!!
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentUpdatePlannerActivityDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            // TODO: lógica de atualização de atividades
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
